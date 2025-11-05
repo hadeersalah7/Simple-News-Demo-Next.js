4f4de8a1eca67882e0e769a5461187ae62ff884b
Hadeer Salah <hadeersalah127@gmail.com>
2025-11-05 09:32:06 +0200

    adding and displaying dummy news in the pages

--- app/news/[newsMainPage]/page.tsx
+++ app/news/[newsMainPage]/page.tsx
@@ -1,11 +1,22 @@
 import Link from "next/link";
+import { DUMMY_NEWS } from "../../../dummy-news";
 
 
 export default function NewsSinglePage(props: { params: { newsMainPage: string } }) {
+    const newsSlug = props.params.newsMainPage;
+    const newsItem = DUMMY_NEWS.find((news) => news.slug === newsSlug);
+
+    if (!newsItem) {
+        return <h1 style={{ color: "red" }}>News Item Not Found!</h1>;
+    }
     return (
-        <>
-            <h1>News Detail Page</h1>
-            <h6>News Id: {props.params.newsMainPage}</h6>
-        </>
+        <article className="news-article">
+            <header>
+                <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                <h1>{newsItem.title}</h1>
+                <time dateTime={newsItem.date}>{newsItem.date}</time>
+            </header>
+            <p>{newsItem.content}</p>
+        </article>
     );
 }
--- components/HeaderPage.tsx
+++ components/HeaderPage.tsx
@@ -7,11 +7,15 @@ interface HeaderPageProps {
 
 const HeaderPage = () => {
     return (
-        <header>
+        <header id="main-header">
+            <div id="logo">
+                <Link href="/">NextNews</Link>
+            </div>
             <nav>
                 <ul>
-                    <li><Link href={"/"}>Home</Link></li>
-                    <li><Link href={`/news`}>News Page</Link></li>
+                    <li>
+                        <Link href="/news">News</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
