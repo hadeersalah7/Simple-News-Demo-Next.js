adb3a3f49561cd88a772c7d629b5f34e4ce148a4
Hadeer Salah <hadeersalah127@gmail.com>
2025-11-10 11:46:12 +0200

    using Suspense to show some loading fallback instead of relying only on loading.js

--- app/(content)/archive/@archive/[[...filter]]/page.tsx
+++ app/(content)/archive/@archive/[[...filter]]/page.tsx
@@ -1,50 +1,72 @@
+import { JSX, Suspense } from "react";
 import Link from "next/link";
 import NewsItem from "../../../../../components/NewsItem";
 import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "../../../../../lib/news";
 import { DummyNewsItem } from "../../../../../dummy-news";
 
+const FilterNews = async ({ year, month }: { year: string, month: string }): Promise<JSX.Element> => {
+    let news: DummyNewsItem[]
+    if (year && !month) {
+        news = await getNewsForYear(year)
+    } else if (year && month) {
+        news = await getNewsForYearAndMonth(year, month);
+    }
+    let newsContent = <p>No News Found For The Selected Period</p>
+    if (news && news.length > 0) {
+        newsContent = <NewsItem news={news} />
+    }
+    return newsContent;
+}
+
+
+const FilterLinks = async ({ year, month }: { year: string, month: string }): Promise<JSX.Element> => {
+    const availableYears = await getAvailableNewsYears()
+    let links = availableYears;
+
+
+    if (year && !month) {
+        links = getAvailableNewsMonths(year);
+    }
+
+    if (year && month) {
+        links = [];
+    }
+
+    if ((year && !availableYears.includes(year)) || (month && !getAvailableNewsMonths(year).includes(month))) {
+        throw new Error("invalid filter")
+    }
+
+    return (
+        <header id="archive-header">
+            <nav>
+                <ul>
+                    {links.map((link) => {
+                        const href = year ? `/archive/${year}/${link}` : `/archive/${link}`;
+                        return (
+                            <li key={link}>
+                                <Link href={href}>{link}</Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
 export default async function ArchiveFilterYear({ params }: { params: { filter: string } }) {
     const filter = params.filter;
     const selectedYear = filter?.[0]
     const selectedMonth = filter?.[1]
-    let news: DummyNewsItem[]
-    let links = await getAvailableNewsYears();
-    let newsContent = <p>No News Found For The Selected Period</p>
 
-    if (selectedYear && !selectedMonth) {
-        news = await getNewsForYear(selectedYear)
-        links = getAvailableNewsMonths(selectedYear);
-    }
 
-    if (selectedYear && selectedMonth) {
-        news = await getNewsForYearAndMonth(selectedYear, selectedMonth);
-        links = [];
-    }
-    if (news && news.length > 0) {
-        newsContent = <NewsItem news={news} />
-    }
-
-    const availableYears = await getAvailableNewsYears()
-    if ((selectedYear && !availableYears.includes(selectedYear)) || (selectedMonth && !getAvailableNewsMonths(selectedYear).includes(selectedMonth))) { 
-        throw new Error("invalid filter")
-    }
     return (
         <>
-            <header id="archive-header">
-                <nav>
-                    <ul>
-                        {links.map((link) => {
-                            const href = selectedYear ? `/archive/${selectedYear}/${link}` : `/archive/${link}`;
-                            return (
-                                <li key={link}>
-                                    <Link href={href}>{link}</Link>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </nav>
-            </header>
-            {newsContent}
+
+            <Suspense fallback={<h4>Loading Filter... Please Wait!</h4>}>
+                <FilterLinks year={selectedYear} month={selectedMonth}/>
+                <FilterNews year={selectedYear} month={selectedMonth} />
+            </Suspense>
         </>
 
     );
